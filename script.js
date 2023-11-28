adefe4a64a6196d5f7feb5a5f84e42a02487bfa4
Ofa Partick <ofapatrick04@gmail.com>
2023-11-29 00:19:38 +0100

    that is all i will do

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function toggleLevels(element) {
     element.classList.toggle('active');
 }
+
+function coursesDrop() {
+    const coursesMenu = document.getElementById("courses-menu");
+
+    coursesMenu.style.display = "block";
+}
+
+function coursesPick() {
+    const coursesMenu = document.getElementById("courses-menu");
+
+    coursesMenu.style.display = "none";
+
+}
+
+document.getElementById('courses').addEventListener('click', coursesDrop);
+document.getElementById('courses-container').addEventListener('mouseleave', coursesPick);
